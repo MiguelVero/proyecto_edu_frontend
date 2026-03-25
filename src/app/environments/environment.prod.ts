35cb06baf7f2838946e1565a1351be2eabc0aea2
MiguelVero <miguelangelvelasquezrosas1@gmail.com>
2026-03-25 13:59:30 -0500

    url del backend

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: '/api',
+  apiUrl: 'https://proyectoeduback-production.up.railway.app/api',
   baseUrl: '', // En producción será relativo o la URL del dominio
   wsUrl: 'wss://tu-dominio.railway.app'
 };
